0023953d2c188cdb738bc845659832132a4727a4
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-03-03 17:15:07 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 172 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,43 +1,51 @@
-
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
         for(int t=1; t<=10; t++){
-            int n=sc.nextInt();
-            sc.nextLine();
-            String line=sc.nextLine();
-            int result=1;
+            int n=Integer.parseInt(br.readLine());
+            char[] line=br.readLine().toCharArray();
+            Stack<Character> stack=new Stack<>();
+            boolean check=true;
 
-            Deque<Character> deque=new ArrayDeque<>();
+            for(int i=0; i<n; i++){
+                char cur=line[i];
+                if(cur=='{'||cur=='('||cur=='['||cur=='<'){
+                    stack.push(cur);
+                }else{
+                    if(cur=='}') {
+                        if(stack.pop()!='{') {
+                            check=false;
+                            break;
+                        }
 
-            for(char c:line.toCharArray()){
-                //여는 괄호부터 스택에 넣어주기
-                if (c == '(' || c == '{' || c == '[' || c == '<') {
-                    deque.push(c);
-                }else{ //닫는 괄호일때 하나씩 빼면서 비교하기
-                    if(deque.isEmpty()){ //닫는 괄호가 나왔는데 스택이 비어잇을 경우
-                        result=0;
-                        break;
+                    }if(cur==')'){
+                        if(stack.pop()!='(') {
+                            check=false;
+                            break;
+                        }
+                    }if(cur==']'){
+                        if(stack.pop()!='[') {
+                            check=false;
+                            break;
+                        }
+                    }if(cur=='>'){
+                        if(stack.pop()!='<') {
+                            check=false;
+                            break;
+                        }
                     }
-                    char top=deque.pop();
-                    if((c==')'&&top!='(')|| (c=='>'&&top!='<')|| (c=='}'&&top!='{'||(c==']'&&top!='['))){
-                        result=0;
-                        break;
-                    }
-                    
+
+
                 }
+
             }
-            if(!deque.isEmpty()){
-                result=0;
-            }
-            System.out.println("#"+t+" "+result);
+            System.out.println("#"+t+" "+(check?1:0));
         }
     }
-
 }
