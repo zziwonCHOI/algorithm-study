2a2c26399d48f4b95f5f521f1359847de1da13bf
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-03-04 17:06:18 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,40 +1,49 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static boolean[] visited;
-    static int n;
-    static int cnt=0;
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
 
-        n=sc.nextInt();
-        int m=sc.nextInt();
-        sc.nextLine();
+	static int v,e;
+	static int[][] graph;
+	static boolean[] visited;
+	static int cnt;
+	public static void main(String[] args) throws IOException {
 
-        arr=new int[n+1][n+1];
-        visited=new boolean[n+1];
-        for(int i=1; i<=m; i++){
-            int a=sc.nextInt();
-            int b=sc.nextInt();
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		v=Integer.parseInt(br.readLine());
+		e=Integer.parseInt(br.readLine());
+		
+		graph=new int [v+1][v+1];
+		
+		for(int i=0; i<e; i++) {
+			st=new StringTokenizer(br.readLine());
+			int from=Integer.parseInt(st.nextToken());
+			int to=Integer.parseInt(st.nextToken());
+			
+			graph[from][to]=graph[to][from]=1;
+		}
+		
+		visited=new boolean[v+1];
+		cnt=0;
+		dfs(1);
+		
+		System.out.println(cnt);
+		
+	}
+	
+	public static void dfs(int start) {
+		visited[start]=true;
+		for(int i=1; i<=v; i++) {
+			if(graph[start][i]==1&&!visited[i]) {
+				cnt++;
+				dfs(i);
+			}
+		}
+	}
 
-            arr[a][b]=arr[b][a]=1;
-        }
-        dfs(1);
-
-        System.out.println(cnt);
-    }
-
-    public static void dfs(int start){
-        visited[start]=true;
-
-        for(int i=1; i<=n; i++){
-            if(arr[start][i]==1&& !visited[i]){
-                cnt++;
-                dfs(i);
-            }
-        }
-    }
 }
