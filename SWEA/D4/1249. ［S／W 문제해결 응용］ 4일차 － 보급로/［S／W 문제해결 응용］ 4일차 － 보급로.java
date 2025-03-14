0d2987ebc12bf5013db1ea8427bc94791aca16e6
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-03-14 14:37:15 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 167 ms, Memory: 36,836 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,71 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Solution {
-    static final int INF=999999;
-    static int[] dx={0,1,0,-1};
-    static int[] dy={1,0,-1,0};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int T=Integer.parseInt(br.readLine());
+	static int[][] arr;
+	static int n;
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { 1, 0, -1, 0 };
+	static int[][] distance;
+	private static final int INF = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		int T = Integer.parseInt(br.readLine());
 
-        for(int t=1; t<=T; t++){
-            int n=Integer.parseInt(br.readLine());
-            int[][] arr=new int[n][n];
+		for (int t = 1; t <= T; t++) {
+			n = Integer.parseInt(br.readLine());
+			arr = new int[n][n];
+			distance=new int[n][n]; 
+			for (int i = 0; i < n; i++) {
+				String s = br.readLine();
+				for (int j = 0; j < n; j++) {
+					arr[i][j] = s.charAt(j) - '0';
+					distance[i][j] = INF; // 초기값 설정
+				}
+			}
 
-            for(int i=0; i<n; i++){
-                String[] line=br.readLine().split("");
-                for(int j=0; j<n; j++){
-                    arr[i][j]=Integer.parseInt(line[j]);
-                }
-            }
+			bfs(0, 0);
 
-            int[][] distance=new int[n][n];
+			System.out.println("#" + t + " " + distance[n - 1][n - 1]);
+		}
+	}
 
-            for(int i=0; i<n; i++){
-                Arrays.fill(distance[i],INF);
-            }
+	public static void bfs(int x, int y) {
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
 
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				return o1[2] - o2[2];
+			}
 
-            PriorityQueue<int[]> pq=new PriorityQueue<>(new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return o1[2]-o2[2];
-                }
-            });
+		});
 
-            distance[0][0]=0;
-            pq.add(new int[]{0,0,0});
+		pq.add(new int[] { x, y, arr[x][y] });
+		distance[x][y]=arr[x][y];
+		
+		while (!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			for (int k = 0; k < 4; k++) {
+				int curx = cur[0] + dx[k];
+				int cury = cur[1] + dy[k];
+				int cost = cur[2];
 
-            while(!pq.isEmpty()){
-                int[] cur=pq.poll();
-                int curX=cur[0];
-                int curY=cur[1];
-                int cost=cur[2];
+				
+				if (curx < 0 || cury < 0 || curx >= n || cury >= n)
+					continue;
 
-                if(cost>distance[curX][curY]) continue;
+				int newCost = cost + arr[curx][cury];
+				if (newCost < distance[curx][cury]) {
+					distance[curx][cury] = newCost;
+					pq.add(new int[] { curx, cury, newCost });
 
-                for(int k=0; k<4; k++){
-                    int x=curX+dx[k];
-                    int y=curY+dy[k];
-                    if(x>=0&&y>=0&&x<n&&y<n){
-                        int d=distance[curX][curY]+arr[x][y];
-                        if(d<distance[x][y]){
-                            distance[x][y]=d;
-                            pq.add(new int[]{x,y,d});
-                        }
-                    }
+				}
 
-                }
-            }
+			}
+		}
 
-            System.out.println("#"+t+" "+distance[n-1][n-1]);
-
-        }
-
-
-    }
+	}
 }
