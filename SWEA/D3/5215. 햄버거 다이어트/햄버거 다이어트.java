816567925ce99c29c36fddcca3a131eb1966769f
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-03-15 18:36:35 +0900

    [D3] Title: 햄버거 다이어트, Time: 143 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,53 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int[] taste;
-    static int[] cal;
     static int n,l;
-    static int max;
+    static int[] scores;
+    static int[] kcals;
+    static int maxTotal;
+    static boolean[] isSelected;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in)) ;
         StringTokenizer st;
 
         int T=Integer.parseInt(br.readLine());
         for(int t=1; t<=T; t++){
             st=new StringTokenizer(br.readLine());
-            n=Integer.parseInt(st.nextToken());
-            l=Integer.parseInt(st.nextToken());
 
-            taste=new int[n];
-            cal=new int[n];
+            n=Integer.parseInt(st.nextToken()); //재료의 수
+            l=Integer.parseInt(st.nextToken()); //제한 칼로리
 
+           scores=new int[n];
+           kcals=new int[n];
             for(int i=0; i<n; i++){
                 st=new StringTokenizer(br.readLine());
-                taste[i]=Integer.parseInt(st.nextToken());
-                cal[i]=Integer.parseInt(st.nextToken());
+                int score=Integer.parseInt(st.nextToken());
+                int kal=Integer.parseInt(st.nextToken());
+
+                scores[i]=score;
+                kcals[i]=kal;
             }
+            maxTotal=Integer.MIN_VALUE;
+            isSelected=new boolean[n];
+            subSet(0,0,0);
 
-            max=0;
-            comb(0,0,0);
-            System.out.println("#"+t+" "+max);
+            System.out.println("#"+t+" "+maxTotal);
+
         }
-
     }
 
-    public static void comb(int cnt, int calSum, int tasteSum){
-        if(calSum>l){
-            return;
-        }
-        if(cnt==n){
-            //지금 까지 초합의 칼로리 합이 최대를 넘지 않았으면 max값을 갱신한다. 
-            if(calSum<=l){
-                max=Math.max(max,tasteSum);
-            }
+    public static void subSet(int idx, int sum, int kcal){
+        if(kcal>l){//정해진 칼로리를 초과하는 경우
             return;
         }
 
-        comb(cnt+1, calSum+cal[cnt],tasteSum+taste[cnt]);
-        comb(cnt+1, calSum,tasteSum);
-    }
+        //모든 재료를 선택했을 경우
+        if(idx==n){
+            maxTotal=Math.max(sum,maxTotal);
+            return;
+        }
 
+        //현재 원소를 선택하는 경우
+        subSet(idx+1, sum+scores[idx],kcal+kcals[idx] );
+        //현재 원소를 선택하지 않는 경우
+        subSet(idx+1, sum,kcal );
+
+    }
 }
