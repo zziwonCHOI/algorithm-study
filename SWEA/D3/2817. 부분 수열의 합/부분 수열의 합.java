7dfa0ab42fa44d0851cb9409f7fd9f8beddf9043
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-03-15 21:57:35 +0900

    [D3] Title: 부분 수열의 합, Time: 155 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -4,44 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static  int[] arr;
-    static int n,k,cnt;
-    static boolean[] isSelecte;
+    static int n,k;
+    static int[] arr;
+    static int cnt;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+        
         int T=Integer.parseInt(br.readLine());
         for(int t=1; t<=T;t++){
             st=new StringTokenizer(br.readLine());
             n=Integer.parseInt(st.nextToken());
             k=Integer.parseInt(st.nextToken());
-
             arr=new int[n];
             st=new StringTokenizer(br.readLine());
-            for(int i=0; i<n; i++){
+            for(int i=0; i<n;i++){
                 arr[i]=Integer.parseInt(st.nextToken());
             }
-
             cnt=0;
-            comb(0,0);
+            subset(0,0);
+
             System.out.println("#"+t+" "+cnt);
         }
-
-
     }
-
-
-    public static void comb(int index,int sum){
-        if(index==n){
+    
+    public static void subset(int idx, int sum){
+        //모든 숫자를 선택했을 경우
+        if(idx==n){
             if(sum==k){
                 cnt++;
             }
             return;
         }
-
-        comb(index+1,sum+arr[index]);
-        comb(index+1,sum);
-
+        
+        if(sum==k){
+            cnt++;
+            return;
+        }
+        //현재 값을 선택하는 경우
+        subset(idx+1,arr[idx]+sum);
+        //현재 값을 선택하지 않는 경우
+        subset(idx+1,sum);
     }
 }
