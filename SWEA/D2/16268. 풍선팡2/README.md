d04e2326afce977bcf13af982e569ae59a86592c
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-03-20 16:48:02 +0900

    [D2] Title: 풍선팡2, Time: 116 ms, Memory: 29,312 KB -BaekjoonHub

---
+++ SWEA/D2/16268. 풍선팡2/README.md
@@ -0,0 +1,15 @@
+# [D2] 풍선팡2 - 16268 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYYlGU56XOkDFARc) 
+
+### 성능 요약
+
+메모리: 29,312 KB, 시간: 116 ms, 코드길이: 1,201 Bytes
+
+### 제출 일자
+
+2025-03-20 16:47
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int n,m;
+	static int[][] arr;
+	static int[] dx= {0,1,0,-1};
+	static int[] dy= {1,0,-1,0};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T=Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			st=new StringTokenizer(br.readLine());
+			n=Integer.parseInt(st.nextToken());
+			m=Integer.parseInt(st.nextToken());
+			arr=new int[n][m];
+			for(int i=0; i<n; i++) {
+				st=new StringTokenizer(br.readLine());
+				for(int j=0; j<m; j++) {
+					arr[i][j]=Integer.parseInt(st.nextToken());
+				}
+			}
+			int maxCnt=Integer.MIN_VALUE;
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<m; j++) {
+					int cnt=pop(i,j);
+					maxCnt=Math.max(maxCnt, cnt);
+				}
+			}
+			
+			System.out.println("#"+t+" "+maxCnt);
+		}
+	}
+
+	public static int pop(int x, int y) {
+		int c=arr[x][y];
+		for(int k=0; k<4; k++) {
+			int nx=x+dx[k];
+			int ny=y+dy[k];
+			if(nx<0||ny<0||nx>=n||ny>=m) continue;
+			
+			c+=arr[nx][ny];
+		}
+		return c;
+	}
+}
