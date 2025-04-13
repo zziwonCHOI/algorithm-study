fda20b6d4137c92468cd9b5b56c7ed7c5937a7fb
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-04-13 16:57:50 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 71 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,47 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
+
     static int[][] arr;
     static boolean[][] visited;
     static int[] dx={0,1,0,-1};
     static int[] dy={1,0,-1,0};
-    static boolean check;
+    static boolean isPossible;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
+        
+
         for(int t=1; t<=10; t++){
             br.readLine();
-
             arr=new int[16][16];
-            for(int i=0; i<16; i++){
-                String[] line=br.readLine().split("");
+            for(int i=0;i<16; i++){
+                String line=br.readLine();
                 for(int j=0; j<16; j++){
-                    arr[i][j]=Integer.parseInt(line[j]);
+                    arr[i][j]=line.charAt(j)-'0';
                 }
             }
 
             visited=new boolean[16][16];
-            check=false;
-            dfs(1,1);
-            System.out.print("#"+t+" ");
-            System.out.println(check?1:0);
+            isPossible=false;
+            bfs(1,1);
+
+            System.out.println("#"+t+" "+(isPossible?1:0));
         }
     }
-    public static void dfs(int startX, int startY){
-        visited[startX][startY]=true;
 
-        for(int k=0; k<4; k++){
-            int x=startX+dx[k];
-            int y=startY+dy[k];
-            if(arr[x][y]==3){
-                check=true;
-                return;
-            }
-            if(x>=0&&y>=0&&x<16&&y<16&&arr[x][y]!=1&&!visited[x][y]){
-                dfs(x,y);
+    public static void bfs(int x, int y){
+        Queue<int[]> q=new LinkedList<>();
+
+        q.add(new int[] {x,y});
+        visited[x][y]=true;
+
+        while(!q.isEmpty()){
+            int[] cur=q.poll();
+            for(int k=0; k<4; k++){
+                int nx=cur[0]+dx[k];
+                int ny=cur[1]+dy[k];
+                if(nx<0||ny<0||nx>=16||ny>=16||arr[nx][ny]==1) continue;
+
+                if(arr[nx][ny]==3){
+                    isPossible=true;
+                    return;
+                }
+
+                if(arr[nx][ny]==0 && !visited[nx][ny]){
+                    visited[nx][ny]=true;
+                    q.add(new int[] {nx,ny});
+                }
             }
         }
     }
